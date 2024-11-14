0b6a407b3131766ad6e7e7eb2ffc425fc72f6f2f
Rohit Chand <chandrohitsumit@gmail.com>
2024-11-15 05:12:44 +0530

    Initial commit of ik project to rozgar repository

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,17 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+// vite.config.js
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // Allows you to use "@" as an alias for the "./src" folder
     },
   },
-})
+  build: {
+    chunkSizeWarningLimit: 1000, // Optional: Increase warning limit for chunk size
+  },
+});
+
