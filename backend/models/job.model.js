19556bac60ac5bb3a3a6d5f79d4fa6f42279d916
rchand7 <150305815+rchand7@users.noreply.github.com>
2024-11-19 12:24:00 +0530

    Update job.model.js

--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -3,50 +3,53 @@ import mongoose from "mongoose";
 const jobSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true
+        required: true,
     },
     description: {
         type: String,
-        required: true
+        required: true,
     },
-    requirements: [{
-        type: String
-    }],
+    requirements: [
+        {
+            type: String,
+        },
+    ],
     salary: {
-        type: Number,
-        required: true
+        type: mongoose.Schema.Types.Mixed, // Allows any type of value
+        required: true,
     },
-    experienceLevel:{
-        type:Number,
-        required:true,
+    experienceLevel: {
+        type: mongoose.Schema.Types.Mixed, // Allows any type of value
+        required: true,
     },
     location: {
         type: String,
-        required: true
+        required: true,
     },
     jobType: {
         type: String,
-        required: true
+        required: true,
     },
     position: {
         type: Number,
-        required: true
+        required: true,
     },
     company: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Company',
-        required: true
+        ref: "Company",
+        required: true,
     },
     created_by: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+        ref: "User",
+        required: true,
     },
     applications: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Application',
-        }
-    ]
-},{timestamps:true});
+            ref: "Application",
+        },
+    ],
+}, { timestamps: true });
+
 export const Job = mongoose.model("Job", jobSchema);
