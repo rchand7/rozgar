6a251d459fbf2c1bf5f9e6a3a3e47054a9e08c2b
rchand7 <150305815+rchand7@users.noreply.github.com>
2024-11-19 11:42:03 +0530

    Update isAuthenticated.js

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -5,7 +5,7 @@ const isAuthenticated = async (req, res, next) => {
         const token = req.cookies.token;
         if (!token) {
             return res.status(401).json({
-                message: "User not authenticated",
+                message: "User not authenticated, sign up to https://rozgar-yyt2.onrender.com",
                 success: false,
             })
         }
