b429720ae10ce4c240a865c16da967531e57d995
rchand7 <150305815+rchand7@users.noreply.github.com>
2024-11-19 13:14:07 +0530

    Update isAuthenticated.js

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -4,27 +4,22 @@ const isAuthenticated = async (req, res, next) => {
     try {
         const token = req.cookies.token;
         if (!token) {
-            // Redirect to the signup page if the user is not authenticated
-            return res.redirect('https://rozgar-yyt2.onrender.com'); // You can change the URL if needed
-        }
-        
-        const decode = await jwt.verify(token, process.env.SECRET_KEY);
-        if (!decode) {
             return res.status(401).json({
-                message: "Invalid token",
+                message: "User not authenticated, sign up to https://rozgar-yyt2.onrender.com",
                 success: false,
-            });
+            })
         }
-
+        const decode = await jwt.verify(token, process.env.SECRET_KEY);
+        if(!decode){
+            return res.status(401).json({
+                message:"Invalid token",
+                success:false
+            })
+        };
         req.id = decode.userId;
-        next();  // Proceed to the next middleware or route handler
+        next();
     } catch (error) {
         console.log(error);
-        return res.status(500).json({
-            message: "An error occurred",
-            success: false,
-        });
     }
 }
-
 export default isAuthenticated;
